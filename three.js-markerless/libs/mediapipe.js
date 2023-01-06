4d96ac293bbfe83e6a71c625ee047f3df41b6a56
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2023-01-06 10:57:51 +0530

    Update mediapipe.js

--- three.js-markerless/libs/mediapipe.js
+++ three.js-markerless/libs/mediapipe.js
@@ -55,7 +55,7 @@ const config = {
     locateFile: (file) => {
         if (!filesLoaded.includes(file)) {
             console.log("loading..", file);
-            return `${libs}hands/${file}`;
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
         } else {
             console.log("already loaded...", file)
         }
